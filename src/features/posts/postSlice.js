5555f9df837b552ebf85c8004a6509c7a2cb417f
Baraka Mulumia <barakamulumia@gmail.com>
2021-01-11 09:44:47 +0300

    + post slice

---
+++ src/features/posts/postSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+const initialState = [
+  { id: 1, title: 'First Post', content: 'Hello!' },
+  { id: 2, title: 'Second Post', content: 'More Text' },
+]
+
+const postSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {},
+})
+
+export default postSlice.reducer

e37393773c17aefee3ebe3e93aeb50f9d6b2c591
Baraka Mulumia <barakamulumia@gmail.com>
2021-01-11 13:18:17 +0300

    @+functionality -displaying a single post

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 const initialState = [
-  { id: 1, title: 'First Post', content: 'Hello!' },
-  { id: 2, title: 'Second Post', content: 'More Text' },
+  { id: '1', title: 'First Post', content: 'Hello!' },
+  { id: '2', title: 'Second Post', content: 'More Text' },
 ]
 
 const postSlice = createSlice({
